b46607367431b12e23e5b6e5d51486bb9ed593d6
Tobias Wilken <tooangel@tooangel.de>
2014-06-28 23:07:30 +0200

    Remove size

--- src/gunicorn_config.py
+++ src/gunicorn_config.py
@@ -2,7 +2,7 @@ import os
 import multiprocessing
 
 bind = "0.0.0.0:{}".format(os.getenv("PORT", 5000))
-size = int(os.getenv("SIZE", 1))
+# size = int(os.getenv("SIZE", 1))
 # Maximum 5 worker per 128MB
 # workers = min(multiprocessing.cpu_count() * 2 + 1, size * 5)
 workers = 4
